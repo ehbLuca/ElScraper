5883b920177f31c61a45d4bae43cbe722a879ab6
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-05-29 01:48:56 +0200

    added no-clobber option to wget

--- scripts/downloader.sh
+++ scripts/downloader.sh
@@ -10,5 +10,5 @@ while read line
 do 
 	id="$(echo $line | cut -f 1 -d ',')"
 	link="$(echo $line | cut -f 2 -d ',')"
-	wget --no-clobber --quiet --show-progress -O "$img_dir/$id.png" $link 
+	wget --no-clobber --quiet --show-progress -O "$img_dir/$id" $link 
 done < ./img_links.txt
