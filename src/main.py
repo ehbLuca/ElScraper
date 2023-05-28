f27b4a70e7d3e611f383110064675aa61b244b86
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-05-29 01:53:55 +0200

    updated variables

--- src/main.py
+++ src/main.py
@@ -8,6 +8,10 @@ from selenium.webdriver.common.by import By
 import gmrequests
 import queries
 
+# Defining variables
+URL = f"https://www.google.com/maps/place"
+FILE = "../out/img_links.txt"
+
 def setAddresses():
     places = queries.getTable('places')
     for place in places:
@@ -30,9 +34,6 @@ def findImage(driver, selector):
     return driver.find_element(By.CSS_SELECTOR, selector)
 
 def scrapeImages(driver, skip=0):
-    # Defining variables
-    url = f"https://www.google.com/maps/place"
-    output_file = "img_links.txt"
     # selectors
     deny_button_text = 'Alles afwijzen'
     search_button_id = 'searchbox-searchbutton'
@@ -40,7 +41,7 @@ def scrapeImages(driver, skip=0):
 
     # Starting browser
     driver.implicitly_wait(0.5)
-    driver.get(url)
+    driver.get(URL)
     findDenyButton(driver, deny_button_text).click()
 
     # Going through the places
@@ -57,7 +58,7 @@ def scrapeImages(driver, skip=0):
         query = query.replace(" ", "+")
 
         # Search for place on maps
-        driver.get(url + "/" + query)
+        driver.get(URL + "/" + query)
         findSearchButton(driver, search_button_id).click()
         # Let the image load
         time.sleep(4)
@@ -68,7 +69,7 @@ def scrapeImages(driver, skip=0):
             img_url = image.get_attribute("src")
             print(img_url)
             # write the place_id followed by the url of the image to a file seperated by a comma
-            with open(output_file, "a+") as file:
+            with open(FILE, "a+") as file:
                 file.write(f"{place_id}, {img_url}\n")
         except:
             print("No image found.")
